9d7267dfc9a5ad083c0dcd644a170f6941e1cbc9
NicoleChant <nicolechantzi@gmail.com>
2022-11-11 02:51:48 +0200

    removed unnecessary code

--- csgo-scraper/app.py
+++ csgo-scraper/app.py
@@ -16,12 +16,11 @@ class App:
 
     def run(self):
         sampled_time = gauss(1200 , 120)
+        print(sampled_time)
         while True:
             data = self.scraper.parse_data()
-            self.bq.upload_data(data)
-
-            break
-            #self.big_query.upload_data(data)
+            print(data)
+            #self.bq.upload_data(data)
             time.sleep(sampled_time)
 
 
@@ -30,35 +29,6 @@ class BigQuery:
     def __init__(self , table_name : str):
         self.client = bigquery.Client()
         self.table_id = f"{os.environ.get('PROJECT_ID')}.{os.environ.get('DATASET_ID')}.{table_name}"
-
-    def create_table_1(self):
-        schema = [
-            bigquery.SchemaField(
-                "team",
-                "RECORD",
-                mode="NULLABLE",
-                fields=(bigquery.SchemaField("first", "STRING"),
-                        bigquery.SchemaField("second", "STRING")))
-        ]
-        table = bigquery.Table(self.table_id , schema = schema)
-        table = self.client.create_table(table)
-        print("Created table {}.{}.{}".format(table.project,
-                                              table.dataset_id,
-                                              table.table_id))
-
-    def insert_data(self):
-        rows_to_insert = [{
-            "team": {
-                "first": "h",
-                "second": "2"
-            }
-        }]
-
-        errors = self.client.insert_rows_json(self.table_id, rows_to_insert)  # Make an API request.
-        if errors == []:
-            print("New rows have been added.")
-        else:
-            print("Encountered errors while inserting rows: {}".format(errors))
 
     def create_table(self):
         schema = [
@@ -95,10 +65,6 @@ class BigQuery:
     def upload_data(self , data : dict):
         #table = self.client.get_table(self.table_id)
         errors = self.client.insert_rows_json(self.table_id , data)
-        if errors == []:
-            print("New rows have been added.")
-        else:
-            print("Encountered errors while inserting rows: {}".format(errors))
 
     def delete_table(self):
         self.client.delete_table(self.table_id , not_found_ok = True)
@@ -109,9 +75,5 @@ class BigQuery:
 
 if __name__ == "__main__":
     bq = BigQuery("match")
-    #bq.delete_table()
-    #bq.create_table()
-
-    #bq.insert_data()
     app = App()
     app.run()
